900d1ed1e919ccc5802eb5406545e043864bc774
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-08-30 14:19:45 -0700

    modified Though and index in Models, created seeds with index, thought, and uses

--- models/Thought.js
+++ models/Thought.js
@@ -4,11 +4,15 @@ const thoughtSchema = new Schema ({
     thoughtText: {
         type: String,
         required: true,
-        match: '/^.{0,20}$/',       
+        match: '/^.{1,280}$/',       
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
     },
     createdAt: {
-        
+        timestamps: true
     }
 })
 
-module.exports = model('Thought', userSchema);
+module.exports = model('Thought', thoughtSchema);
--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
+const Thought = require("./Thought")
 
 module.exports = {
-    User, 
+    User,
+    Thought,
     
 };
