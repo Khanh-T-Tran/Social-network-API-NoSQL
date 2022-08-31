b8407df6690b68c97d94713e79b8ab8e2e6b77b4
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-08-31 13:35:26 -0700

    delete seeds

--- models/Thought.js
+++ models/Thought.js
@@ -1,18 +1,22 @@
 const { Schema, model } = require('mongoose');
+// import moment module timestamp's format 
+const moment = require('moment')
 
-const thoughtSchema = new Schema ({
+const thoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
-        match: '/^.{1,280}$/',       
-    },
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
+        match: '/^.{1,280}$/',
     },
     createdAt: {
-        timestamps: true
-    }
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
 })
 
 module.exports = model('Thought', thoughtSchema);
--- models/User.js
+++ models/User.js
@@ -2,43 +2,55 @@ const { Schema, model } = require('mongoose');
 // require validator pakage (after npm i validator)
 const validatorPackage = require('validator');
 // Create the blueprint for mongo collection
-const userSchema = new Schema({
-    username: {
-        type: String,
-        Unique: true,
-        required: [true, "Please enter an username"],
-        trim: true
-    },
-    userPassword: {
-        type: String,
-        Unique: true,
-    },
-    email: {
-        type: String,
-        required: [true, "Email address is required"],
-        Unique: true,
-        validate: {
-            validator: validatorPackage.isEmail,
-            message: 'Please provide a valid email',
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            Unique: true,
+            required: [true, "Please enter an username"],
+            trim: true
         },
+        userPassword: {
+            type: String,
+            Unique: true,
+        },
+        email: {
+            type: String,
+            required: [true, "Email address is required"],
+            Unique: true,
+            validate: {
+                validator: validatorPackage.isEmail,
+                message: 'Please provide a valid email',
+            },
+        },
+        // Array of _id values referencing the "Thought" model
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+        // Array of _id values referencing the "User" model (self-reference)
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
     },
-    // Array of _id values referencing the "Thought" model
-    thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Thought",
+    
+    {
+        toJSON: {
+            virtuals: true,
         },
-    ],
-    // Array of _id values referencing the "User" model (self-reference)
-    friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-        },
-    ],
+        id: false,
+    }
+);
 
-    // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
-});
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });
 
 //  create and export the collection so we can use it in other files
 module.exports = model('User', userSchema); // 1st para: name of the collection, 2nd para: the blueprint that we use for this collection.
