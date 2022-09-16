0b1c9d1cf82e21a7e87b939740e27024eb3a8d93
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-09-16 00:29:13 -0700

    got all routes in routes folder worked

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const ObjectId = require('mongodb').ObjectId;
 // const { Router } = require('express');
 const { User,Thought } = require('../../models');
 
 // Create routes for THOUGHTS
 // get route for getting all thoughts
-router.get('/api/thoughts', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const allThoughts = await Thought.find();
         res.status(200).json(allThoughts);
@@ -14,7 +15,7 @@ router.get('/api/thoughts', async (req, res) => {
 });
 
 // get route for getting a thought by ID
-router.get('/api/thoughts/:thoughtId', async (req, res) => {
+router.get('/:thoughtId', async (req, res) => {
     try {
         const oneThought = await Thought.findById({
             _id: req.params.thoughtId,
@@ -26,7 +27,7 @@ router.get('/api/thoughts/:thoughtId', async (req, res) => {
 });
 
 // post route for posting new thoughts
-router.post('/api/thoughts', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const newThought = await Thought.create(req.body);
 
@@ -49,7 +50,7 @@ router.post('/api/thoughts', async (req, res) => {
 });
 
 // put route for updating a thought by its ID
-router.put('/api/thoughts/:thoughtId', async (req, res) => {
+router.put('/:thoughtId', async (req, res) => {
     try {
         const updatedThought = await Thought.findByIdAndUpdate(
             req.params.thoughtId,
@@ -64,7 +65,7 @@ router.put('/api/thoughts/:thoughtId', async (req, res) => {
 });
 
 // delete route for deleting a thought by its ID
-router.delete('/api/thoughts/:thoughtId', async (req, res) => {
+router.delete('/:thoughtId', async (req, res) => {
     try {
         const deletedThought = await Thought.findByIdAndDelete({
             _id: ObjectId(req.params.thoughtId)
@@ -72,6 +73,61 @@ router.delete('/api/thoughts/:thoughtId', async (req, res) => {
 
         res.status(200).json(deletedThought);
     } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+
+
+// Create routes for REACTIONS
+// Post route for posting new reactions
+router.post('/:thoughtId/reactions', async (req, res) => {
+    try {
+        const thought = await Thought.findOneAndUpdate(
+            {
+                _id: req.params.thoughtId
+            },
+            {
+                $addToSet: {
+                    reactions: req.body,
+                }
+            },
+            {
+                new: true,
+            }
+        ).populate('reactions');
+
+        res.status(200).json(thought);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+});
+
+// Delete route for deleting a reaction by ID
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+    try {
+        // console.log("thoughtID line 244",req.params.thoughtId);
+        // console.log("thoughtID line 245",req.params.reactionId);
+        const deletedReaction = await Thought.findOneAndUpdate(
+            {
+                _id: req.params.thoughtId,
+            },
+            {
+                $pull: {
+                    reactions: {
+                        _id: req.params.reactionId,
+                    },
+                },
+                
+            },
+            {
+                runValidators: true,
+                new: true,
+            }).exec();
+// console.log("reaction",deletedReaction);
+        res.status(200).json(deletedReaction);
+    } catch (error) {
+        console.log(error);
         res.status(500).json({ error });
     }
 });
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 // const { Router } = require('express');
 const { User, Thought } = require('../../models');
 // Create routes for USER
@@ -43,35 +43,85 @@ router.get('/:userId', async (req, res) => {
 
 
 // Put route for updating a user by it's ID
-// router.put('/api/users/:userId', async (req, res) => {
-//     try {
-//         // findByIdAndUpdate takes 3 params       
-//         const updatedUser = await User.findByIdAndUpdate(
-//             // 1st one is ID of thing we want to update
-//             req.params.userId,
-//             // 2nd one is what updates we want to make // updated data show on mongoDb but will not show on postman after put request
-//             { ...req.body },
-//             // 3rd one is configuration e.g should we call schema "hooks" // make updated data shows up on postman
-//             {new: true},
-//         );
-//         res.json(updatedUser);
-//     } catch (error) {
-//         res.status(500).json({ error });
-//     }
-// })
+router.put('/:userId', async (req, res) => {
+    try {
+        // findByIdAndUpdate takes 3 params       
+        const updatedUser = await User.findByIdAndUpdate(
+            // 1st one is ID of thing we want to update
+            req.params.userId,
+            // 2nd one is what updates we want to make // updated data show on mongoDb but will not show on postman after put request
+            { ...req.body },
+            // 3rd one is configuration e.g should we call schema "hooks" // make updated data shows up on postman
+            {new: true},
+        );
+        res.json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+})
 
 // Delete route for deleting a user by it's ID
-// router.delete('/api/users/:userId', async (req, res) => {
-//     try {       
-//         const deletedUser = await User.findByIdAndDelete({
-//             _id: ObjectId(req.params.userId),
-//             // cascading thoughts when user is deleted
-//             thoughts: req.params.thoughts,
-//         });
-//         res.json(deletedUser);
-//     } catch (error) {
-//         res.status(500).json({ error });
-//     }
-// })
+router.delete('/:userId', async (req, res) => {
+    try {       
+        const deletedUser = await User.findByIdAndDelete({
+            _id: ObjectId(req.params.userId),
+            // cascading thoughts when user is deleted
+            thoughts: req.params.thoughts,
+        });
+        res.json(deletedUser);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+})
+
+// Create routes for FRIEND
+// post route to add a new friend to a user's friend list
+router.post('/:userId/friends', async (req, res) => {
+    try {
+        console.log(req.params.userId);
+        const newFriend = await User.findOne(req.body);
+        console.log(req.body, newFriend);
+
+        const addedFriend = await User.findByIdAndUpdate(
+            req.params.userId,
+            {
+                $addToSet: {
+                    friends: newFriend._id,
+                }
+            },
+            {
+                new: true,
+            },
+        ).populate('friends');
+
+        res.status(200).json(addedFriend);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+});
+
+// delete route to remove a friend from a user's friend list
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const deletedFriend = await User.findByIdAndUpdate(
+            req.params.userId,
+            {
+                $pull: {
+                    friends: req.params.friendId,
+                }
+            },
+            {
+                new: true,
+            }
+        ).populate('friends');
+        res.status(200).json(deletedFriend);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+});
+
+
 
 module.exports = router; 
